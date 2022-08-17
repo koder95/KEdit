9d8969c05978ddfdba270cb851373e20ecc5482d
Kamil Jan Mularski <kammul@outlook.com>
2022-08-17 19:39:01 +0200

    Update EkitCore.java

--- src/com/hexidec/ekit/EkitCore.java
+++ src/com/hexidec/ekit/EkitCore.java
@@ -862,23 +862,17 @@ public class EkitCore extends ContentPanel implements ActionListener, KeyListene
 			jToolBarStyles = new JToolBar(JToolBar.HORIZONTAL);
 			jToolBarStyles.setFloatable(false);
 
-			initializeMultiToolbars(settings.toolbarSeq);
-
-			// fix the weird size preference of toggle buttons
-			jtbtnViewSource.setPreferredSize(jbtnAnchor.getPreferredSize());
-			jtbtnViewSource.setMinimumSize(jbtnAnchor.getMinimumSize());
-			jtbtnViewSource.setMaximumSize(jbtnAnchor.getMaximumSize());
-		} else if (settings.includeToolBar) {
+			initializeMultiToolbars(toolbarSeq);
+		} else if (includeToolBar) {
 			jToolBar = new JToolBar(JToolBar.HORIZONTAL);
 			jToolBar.setFloatable(false);
 
-			initializeSingleToolbar(settings.toolbarSeq);
-
-			// fix the weird size preference of toggle buttons
-			jtbtnViewSource.setPreferredSize(jbtnAnchor.getPreferredSize());
-			jtbtnViewSource.setMinimumSize(jbtnAnchor.getMinimumSize());
-			jtbtnViewSource.setMaximumSize(jbtnAnchor.getMaximumSize());
+			initializeSingleToolbar(toolbarSeq);
 		}
+		// fix the weird size preference of toggle buttons
+		jtbtnViewSource.setPreferredSize(jbtnAnchor.getPreferredSize());
+		jtbtnViewSource.setMinimumSize(jbtnAnchor.getMinimumSize());
+		jtbtnViewSource.setMaximumSize(jbtnAnchor.getMaximumSize());
 
 		/* Create the scroll area for the text pane */
 		JScrollPane jspViewport = new JScrollPane(jtpMain);
